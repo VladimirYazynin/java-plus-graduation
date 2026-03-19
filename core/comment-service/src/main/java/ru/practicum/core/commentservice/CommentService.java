1feedceb9220defd2882600e8f6fdbc1d887cce5
VladimirYazynin <vladimir.yazynin@inbox.ru>
2026-03-20 01:10:10 +0300

    feat: Добавление заготовок сервисов для разделения монолита

---
+++ core/comment-service/src/main/java/ru/practicum/core/commentservice/CommentService.java
@@ -0,0 +1,12 @@
+package ru.practicum.core.commentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommentService.class, args);
+    }
+}
---
+++ core/request-service/src/main/java/ru/practicum/core/requestservice/RequestService.java
@@ -0,0 +1,12 @@
+package ru.practicum.core.requestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestService.class, args);
+    }
+}
